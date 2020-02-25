2cb14a69add2b6b5237080dd9d32702182f37129
Luan Prates <pratesr21@gmail.com>
2020-02-25 16:55:58 -0300

    Incluindo mais funcionalidads no dashboard

--- web-api-bytebank/lib/screens/dashboard.dart
+++ web-api-bytebank/lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newbytebank/screens/contacts_list.dart';
+import 'package:newbytebank/screens/contacts_list_transf.dart';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -16,45 +17,93 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ContactsList(),
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.white,
-                        size: 24.0,
-                      ),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.0,
-                        ),
-                      )
-                    ],
-                  ),
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                _FeatureItem(
+                  name: 'Contacts',
+                  icon: Icons.people,
+                  onClick: () => _showContactsList(context),
                 ),
-              ),
+                _FeatureItem(
+                  name: 'Transfer',
+                  icon: Icons.monetization_on,
+                  onClick: () => _showContactsListTransf(context),
+                ),
+                _FeatureItem(
+                  name: 'Feed',
+                  icon: Icons.description,
+                  onClick: () => print('Clicou em feed'),
+                ),
+              ],
             ),
-          )
+          ),
         ],
+      ),
+    );
+  }
+}
+
+
+void _showContactsList(BuildContext context){
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => ContactsList(),
+    ),
+  );
+}
+
+void _showContactsListTransf(BuildContext context){
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => ContactsListTransf(),
+    ),
+  );
+}
+
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem({Key key, this.name, this.icon, this.onClick}) : super(key: key);
+
+ // _FeatureItem(this.name, this.icon, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            width: 114,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
