d5fa3ae59a00d1023f3336560904ddb5680ea33f
Luan Prates <pratesr21@gmail.com>
2020-02-25 14:07:03 -0300

    create new project web API ByteBank
    esse novo projeto se comunica com uma api web.

---
+++ web-api-bytebank/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:newbytebank/screens/dashboard.dart';
+
+void main() {
+  runApp(Newbytebank());
+}
+
+class Newbytebank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+
